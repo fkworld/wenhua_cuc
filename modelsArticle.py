91b5be025646b69f399e43786e4831bf7be68008
fengyong <fkworld@foxmail.com>
2016-10-11 18:29:01 +0800

    修改flag tag id的相关接口

--- modelsArticle.py
+++ modelsArticle.py
@@ -8,33 +8,38 @@ class Article(db.Model):
     author=db.Column(db.String(64))
     tag_id=db.Column(db.Integer)
     flag_id=db.Column(db.Integer)
-    update_datetime=db.Column(db.DateTime)
+    create_datetime=db.Column(db.DateTime)
     context_html=db.Column(db.Text)
     ps_html=db.Column(db.Text)
 
-    tags=('通知','展示','交流','说明')
-    flags=('系统文件','展示文件','修改中文件','删除后备份文件')
+    tags=('','通知','展示','交流','说明')
+    flags=('','正常发布文档','修改中文档','系统文档','删除后服务器备份文档')
 
     def get_tag_name(self):
         return self.tags[self.tag_id]
+    
+    def get_flag_name(self):
+        return self.flags[self.flag_id]
 
-    def get_flag_boolean(self):
-        if self.flag_id==0 or self.flag_id==1:
-            return False
-        else:
+    def get_flag_grade(self):
+        if self.flag_id==1:
             return True
-
-    def set_flag_id(self,flag_boolean):
-        if self.flag_id is not None and self.flag_id==0: #系统文件
-            pass
-        elif flag_boolean==False:
-            self.flag_id=1 #展示文件
         else:
-            self.flag_id=2 #修改中文件
+            return False
         
     def set_id(self):
         if self.id is None:
-            self.id=self.update_datetime.strftime('%Y%m%d%H%M%S')
+            self.id=self.create_datetime.strftime('%Y%m%d%H%M%S')
+
+    def delete(self):
+        if self.flag_id!=4:
+            self.flag_id=4
+            self.update_db()
+            return '文章已经从列表中删除，但是还在数据库中有备份'
+        else:
+            db.session.delete(self)
+            db.session.commit()
+            return '文章备份已经从数据库中完全删除，不可恢复'
 
     def update_db(self):
         db.session.add(self)
@@ -47,5 +52,5 @@ class Article(db.Model):
         self.flag_id=form.flag_id.data
         self.context_html=form.context_html.data
         self.ps_html=form.ps_html.data
-        self.update_datetime=datetime.now()
+        self.create_datetime=datetime.now()
         self.set_id()
