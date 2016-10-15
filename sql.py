cab024a1113bbba8b2151f7d01941e0dc6c3ff43
fengyong <fkworld@foxmail.com>
2016-10-15 19:23:48 +0800

    Add a sql line for create database

---
+++ sql.py
@@ -0,0 +1,10 @@
+create table articles(
+    id char(14) not null primary key,
+    title char(64),
+    author char(64),
+    tag_id tinyint,
+    flag_id tinyint,
+    create_datetime datetime,
+    context_html text,
+    ps_html text
+)
