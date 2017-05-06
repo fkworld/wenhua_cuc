53da2af283cfa41c6bce5363486c143676646bef
fengyong <fkworld@foxmail.com>
2017-05-06 16:47:25 +0800

    修正数据库部分的代码，改为用占位符的方式执行sql语句，目的是为了防止sql注入

--- devtest.py
+++ devtest.py
@@ -3,8 +3,11 @@ from sql import SQL
 
 if __name__=='__main__':
     sql = SQL()
-    test = ('fy')
-    cmd = 'select * from admins where account =?'
-    print(cmd)
-    result  = sql.cursor.execute(cmd, test)
-    print(result.fetchall())
+    #cmd = 'select * from admins'
+    #value_list = ['654321',6]
+    #result = sql.cursor.execute(cmd)
+    #print(result.fetchall())
+
+    target_list = ['id', 5]
+    value_list = ['password', '111111']
+    sql.update_line_single('admins',value_list, target_list)
--- sql.py
+++ sql.py
@@ -45,18 +45,19 @@ class SQL(object):
 
     def add_line(self, table_name, value_list):
         # 根据全属性值添加一行数据
-        cmd_part = ['INSERT INTO']
+        # INSERT INTO VALUES (?,?,?,...),(value1,value2,value3,...,) # 注意最后一项必须带逗号
+        cmd_part = ['INSERT']
+        cmd_part.append('INTO')
         cmd_part.append(str(table_name))
         cmd_part.append('VALUES')
         cmd_part.append('(')
-        cmd_part.append(','.join(map(str,value_list))) # map(str,list)的作用是对list中的所有对象作str()处理
+        cmd_part.append(','.join(['?'] * len(value_list))) # 占位符部分
         cmd_part.append(')')
-        cmd_part.append(';')
         step = ' ' # 空格
         cmd_add_line = step.join(cmd_part)
-        print(cmd_add_line)
+        # print(cmd_add_line) # 用来测试输出的命令是否正确
         try:
-            self.cursor.execute(cmd_add_line)
+            self.cursor.execute(cmd_add_line, value_list) # 采用占位符的方式执行语句
             print('ADD LINE SUCESS.')
         except:
             print('ADD LINE FAILED.')
@@ -66,60 +67,70 @@ class SQL(object):
         cmd_part = ['UPDATE']
         cmd_part.append(str(table_name))
         cmd_part.append('SET')
-        cmd_part.append('='.join(map(str,value_list)))
+        cmd_part.append(str(value_list.pop(0)))
+        cmd_part.append('=')
+        cmd_part.append('?')
         cmd_part.append('WHERE')
-        cmd_part.append('='.join(map(str,target_list)))
-        cmd_part.append(';')
+        cmd_part.append(str(target_list.pop(0)))
+        cmd_part.append('=')
+        cmd_part.append('?')
         step = ' '
         cmd_update_line = step.join(cmd_part)
+        print(cmd_update_line)
         try:
-            self.cursor.execute(cmd_update_line)
+            value_list.extend(target_list) # 合并2个列表剩余的部分
+            self.cursor.execute(cmd_update_line, value_list) # 同样采用占位符的方式来防止sql注入
             print('UPDATE LINE SUCESS.')
         except:
             print('UPDATE LINE FAILED.')
 
-    def delete_line(self, table_name, target_value):
+    def delete_line_targetly(self, table_name, target_list):
         # 根据目标删除某一行的数据
         cmd_part = ['DELETE FROM']
         cmd_part.append(str(table_name))
         cmd_part.append('WHERE')
-        cmd_part.append('='.join(map(str,target_list)))
-        cmd_part.append(';')
+        cmd_part.append(str(target_list.pop(0)))
+        cmd_part.append('=')
+        cmd_part.append('?')
         step = ' '
         cmd_delete_line = step.join(cmd_part)
         try:
-            self.cursor.execute(cmd_delete_line)
+            self.cursor.execute(cmd_delete_line, target_list)
             print('DELETE LINE SUCESS.')
         except:
             print('DELETE LINE FAILED.')
         
     def search_line_all(self, table_name):
         # 搜索全表，往后会改成搜索前N项，点击下一页后再搜索N项，以此类推
-        cmd_part = ['SELECT * FROM']
+        cmd_part = ['SELECT']
+        cmd_part.append('*')
+        cmd_part.append('FROM')
         cmd_part.append(str(table_name))
-        cmd_part.append(';')
         step = ' '
         cmd_search_line_all = step.join(cmd_part)
         try:
             self.cursor.execute(cmd_search_line_all)
-            return self.cursor.fetchall() # 返回结果
             print('SEARCH LINE ALL SUCESS.')
+            return self.cursor.fetchall() # 返回结果
         except:
             print('SEARCH LINE ALL FAILED.')
         
     def search_line_targetly(self, table_name, target_list):
         # 根据目标搜索某一行数据
-        cmd_part = ['SELECT * FROM']
+        cmd_part = ['SELECT']
+        cmd_part.append('*')
+        cmd_part.append('FROM')
         cmd_part.append(str(table_name))
         cmd_part.append('WHERE')
-        cmd_part.append('='.join(map(str,target_list)))
-        cmd_part.append(';')
+        cmd_part.append(str(target_list.pop(0)))
+        cmd_part.append('=')
+        cmd_part.append('?')
         step = ' '
         cmd_search_line_targetly = step.join(cmd_part)
-        print(cmd_search_line_targetly)
+        # print(cmd_search_line_targetly)
         try:
-            self.cursor.execute(cmd_search_line_targetly)
-            return self.cursor.fetchall()
+            self.cursor.execute(cmd_search_line_targetly, target_list)
             print('SEARCH LINE TARGETLY SUCESS.')
+            return self.cursor.fetchall()
         except:
             print('SEARCH LINE TARGETLY FAILED.')
